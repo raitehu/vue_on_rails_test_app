566d0e6d1c0081ee96bf85e6c7724a2245018b9a
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2021-02-14 13:08:18 +0900

    first view by vue.js

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,48 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id='app'>
+    <table>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>name</th>
+          <th>birth</th>
+          <th>department</th>
+          <th>gender</th>
+          <th>joined_date</th>
+          <th>payment</th>
+          <th>note</th>
+        </tr>
+        <tr v-for="e in employees" :key="e.id">
+          <td>{{ e.id }}</td>
+          <td>{{ e.name }}</td>
+          <td>{{ e.birth }}</td>
+          <td>{{ e.department }}</td>
+          <td>{{ e.gender }}</td>
+          <td>{{ e.joined_date }}</td>
+          <td>{{ e.payment }}</td>
+          <td>{{ e.note }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+// scriptタグ内でaxiosが使えるようにする
+import axios from 'axios'
+
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
+      // employeesは空配列 -> th要素だけ表示する
+      employees: []
     }
+  },
+  // 仮想DOMが本物のDOMに置き換わる瞬間がmounted
+  // cf. https://jp.vuejs.org/v2/guide/instance.html
+  mounted() {
+    axios.get('/api/v1/employees')
+         .then(response => (this.employees = response.data))
   }
 }
 </script>
